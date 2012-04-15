3651f5ce94328249d0378d0ac5fe6a1d149729a5
David Leatherman <david@leathekd.com>
2012-04-15 13:25:48 -0400

    Bump clj-http version, remove unused files

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject terebi "1.0.0"
   :description "A simple Clojure interface to thetvdb.com's API"
-  :dependencies [[clj-http "0.3.2"]
+  :dependencies [[clj-http "0.3.6"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.zip "0.1.0"]
                  [slingshot "0.10.2"]]
--- src/terebi/fetch.clj
+++
@@ -1,3 +0,0 @@
-(ns terebi.fetch
-
-  )
